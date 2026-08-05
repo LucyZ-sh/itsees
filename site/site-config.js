37377c82a32b4e56c875632493ff652b9bc5e9e8
LucyZ-sh <zlqlucy93@gmail.com>
2026-08-05 23:00:38 +0800

    Update website for Beta 6

--- site/site-config.js
+++ site/site-config.js
@@ -1,8 +1,7 @@
 window.ITSEES_SITE_CONFIG = Object.freeze({
   downloadEnabled: true,
-  downloadUrl: "https://github.com/LucyZ-sh/itsees/releases/download/v0.1.0-beta.5/Itsees-0.1.0-beta.5-arm64.dmg",
+  downloadUrl: "https://github.com/LucyZ-sh/itsees/releases/download/v0.1.0-beta.6/Itsees-0.1.0-beta.6-arm64.dmg",
   githubUrl: "https://github.com/LucyZ-sh/itsees",
-  skinUrl: "https://github.com/LucyZ-sh/itsees-codex-skin",
   supportUrl: "https://github.com/LucyZ-sh/itsees/issues",
   privacyUrl: "./privacy.html",
   canonicalUrl: "https://lucyz-sh.github.io/itsees/site/",
